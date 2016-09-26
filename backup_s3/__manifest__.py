0abfe485a892daef64a360d85c260b88cdafd00b
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-09-26 17:06:58 -0300

    Módulos de backup e NFSe de Florianopolis

---
+++ backup_s3/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': 'Trustcode Backup Simples',
+    'summary': """Trustcode simples ferramenta de backup""",
+    'version': '8.0',
+    'category': 'Tools',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    "description": """
+        Este módulo permite configurar backup das bases
+        de dados para rodar periodicamente, e integra com o Amazon S3
+    """,
+    'depends': [
+        'base',
+    ],
+    'data': [
+        "views/backup_s3.xml",
+        "security/ir.model.access.csv"
+    ],
+    'application': True,
+    'auto_install': False
+}
