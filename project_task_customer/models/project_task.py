e401c578d56265cabc19039ca44f42d7b2c2260f
Danimar <danimaribeiro@gmail.com>
2016-11-16 18:37:54 -0200

    Arquivo de dependencias, requirements e correção de build

--- project_task_customer/models/project_task.py
+++ project_task_customer/models/project_task.py
@@ -3,8 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import api, fields, models, tools
-from datetime import datetime
+from odoo import models
 
 
 class ProjectTask (models.Model):
