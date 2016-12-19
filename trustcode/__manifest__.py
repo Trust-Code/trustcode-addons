4869e202109ce1de6319d6c771ac625fc240331a
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-12-19 19:06:56 -0200

    Simple module to block undesired installs

---
+++ trustcode/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Base Trustcode',
+    'description': "Módulo base da Trustcode",
+    'version': '10.0.1.0.0',
+    'category': "Administration",
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    'depends': [
+        'br_base',
+    ],
+}
