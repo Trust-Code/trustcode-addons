9d9efe665faa13d2391acdb770f4d0394dd9c332
Alessandro Martini <alessandrofmartini@gmail.com>
2016-12-22 17:18:03 -0200

    Adicionado teste para correio_send_shipping

--- delivery_correios/tests/test_delivery.py
+++ delivery_correios/tests/test_delivery.py
@@ -38,6 +38,11 @@ class TestDeliveryCorreios(TransactionCase):
             'zip': '27336-400',
         }
         self.partner = self.env['res.partner'].create(partner)
+        self.company = self.env['res.company'].create({
+            'legal_name': 'Nome Legal',
+            'name': 'Company 1',
+            'cnpj_cpf': '1234567890123234',
+        })
         product_uom = {
             'name': 'UOM',
             'category_id': self.env['product.uom.categ'].create(
@@ -204,3 +209,78 @@ calcular_preco_prazo')
             tracks_link,
             ['/web#min=1&limit=80&view_type=list&model=delivery.correios.\
 postagem.plp&action=396'])
+
+    @patch('odoo.addons.delivery_correios.models.delivery.\
+check_for_correio_error')
+    @patch('odoo.addons.delivery_correios.models.delivery.\
+calcular_preco_prazo')
+    @patch('odoo.addons.delivery_correios.models.delivery.\
+solicita_etiquetas_com_dv')
+    def test_correios_send_shipping(self, etiquetas, preco, erro):
+        calcular_preco_prazo = os.path.join(xmls, 'calcular_preco_prazo.xml')
+        with open(calcular_preco_prazo, 'r') as correio_return_xml:
+            preco.return_value = objectify.fromstring(
+                correio_return_xml.read())
+        etiquetas.return_value = ['DL760237272BR']
+        erro.return_value = None
+        move_line = [(0, 0, {
+            'name': 'Move 1',
+            'product_id': self.produto.id,
+            'product_uom_qty': 1.0,
+            'product_uom': self.product_uom.id,
+            'state': 'draft',
+        })]
+        pack_operation = [(0, 0, {
+            'qty_done': 0,
+            'product_qty': 1,
+            'location_id': 1,
+            'location_dest_id': 1,
+            'product_id': self.produto.id,
+        })]
+        picking = self.env['stock.picking'].create({
+            'name': 'Picking 1',
+            'partner_id': self.partner.id,
+            'move_lines': move_line,
+            'location_id': 1,
+            'location_dest_id': 1,
+            'picking_type_id': 1,
+            'pack_operation_product_ids': pack_operation,
+        })
+        entrega = self.env['delivery.carrier'].create({
+            'name': 'Metodo 1',
+            'delivery_type': 'correios',
+            'margin': 0,
+            'integration_level': 'rate_and_ship',
+            'correio_login': 'sigep',
+            'correio_password': 'n5f9t8',
+            'cod_administrativo': '08082650',
+            'num_contrato': '9912208555',
+            'cartao_postagem': '0057018901',
+            'ambiente': 1,
+        })
+        servico = self.env['delivery.correios.service'].create({
+            'ano_assinatura': '2016',
+            'name': 'Serviço 1',
+            'code': '40215',
+            'identifier': 'foo bar baz',
+            'delivery_id': entrega.id,
+        })
+        entrega.write({
+            'service_id': servico.id,
+        })
+        self.sale_order.write({
+            'carrier_id': entrega.id
+        })
+        send = entrega.correios_send_shipping(picking)
+        self.assertTrue(len(send) == 1)
+        exact_price = send[0]['exact_price']
+        track_ref = send[0]['tracking_number']
+        self.assertEqual(track_ref, 'DL760237272BR')
+        self.assertEqual(exact_price, 42)
+        postagem = self.env['delivery.correios.postagem.objeto'].search([])
+        self.assertEqual(1, len(postagem))
+        self.assertEqual(postagem.name, 'DL760237272BR')
+        plp = self.env['delivery.correios.postagem.plp'].search([])
+        self.assertEqual(1, len(plp))
+        self.assertEqual(plp.state, 'draft')
+        self.assertEqual(plp.total_value, 42)
