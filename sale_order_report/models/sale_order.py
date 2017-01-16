eaf48fa4307846fd347c761ab494d4c83f5699fd
Mackilem <mack.vdl@gmail.com>
2017-01-16 13:18:28 -0200

    Inserido uma forma de flexibilizar a descrição do layout

---
+++ sale_order_report/models/sale_order.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# © 2017 Mackilem Van der Laan, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
+
+class SaleOrderReport(models.Model):
+    _name = 'sale.order.report'
+    _description = u'Sale Order Report'
+
+
+    name = fields.Char (string='Nome')
+    description = fields.Html('Description')
+    product_type = fields.Selection([(u'product',u'Produto'),
+                                    (u'service',u'Serviço'),
+                                    ])
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def _description_default(self):
+        return self.env['sale.order.report'].search([],limit=1)
+
+    description_report = fields.Many2one('sale.order.report',
+        string='Report Description',
+        help=u'Select a custom description report.',
+        default=_description_default
+    )
