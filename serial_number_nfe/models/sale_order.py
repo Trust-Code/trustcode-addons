01e33ed258a032b0d67b729297f26f69f03addc6
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-01-26 09:12:05 -0200

    Módulo que vincula o número de série e validade ao campo informações adicionais produto

---
+++ serial_number_nfe/models/sale_order.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.multi
+    def _prepare_invoice_line(self, qty):
+        res = super(SaleOrderLine, self)._prepare_invoice_line(qty)
+
+        info = ''
+        for move in self.procurement_ids.move_ids:
+            # Verifica a entrega mais recente finalizada e adiciona os lotes
+            if move.state == 'done':
+                for lote in move.lot_ids:
+                    expiry = fields.Datetime.from_string(lote.life_date)
+                    info += u"Lote/Série: %s / Vencimento: %s\n" % (
+                        lote.name, expiry.strftime("%d/%m/%Y")
+                    )
+                break
+        res['informacao_adicional'] = info
+        return res
