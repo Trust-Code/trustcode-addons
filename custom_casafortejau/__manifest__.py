4749c7c6c3ee178132d8a9fe40331fec3706f8be
martini97 <alessandrofmartini@gmail.com>
2017-02-16 19:21:48 -0200

    Customizações para Casa Forte Jaú

---
+++ custom_casafortejau/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2015 Alessandro Fernandes Martini, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': 'Customizações para Casa Forte Jaú',
+    'description': 'Customizações para Casa Forte Jaú',
+    'summary': """Alterações desenvolvidas para Casa Forte Jaú""",
+    'version': '10.0.1.0.0',
+    'category': 'Customization',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+        'Mackilem Van der Laan Soares <mack.vdl@gmail.com>'
+    ],
+    'depends': [
+        'br_crm_zip',
+    ],
+    'data': [
+        'views/crm_lead.xml',
+        'views/crm_stage.xml',
+    ],
+}
---
+++ custom_casafortejau/models/crm_stage.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# © 2015 Alessandro Fernandes Martini, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class CrmStage(models.Model):
+    _inherit = 'crm.stage'
+
+    maximum_days = fields.Integer(string="Máximo de dias neste estágio")
