4568a1bd3609c59f75b0a0b9b91de9c0b5036347
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-03-20 17:27:48 -0300

    10.0 task materials (#37)
    * NEW: Project Task Materials

---
+++ project_task_materials/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Materiais em Terafas',
+    'description': "Materiais em Tarefas",
+    'summary': "Materiais em Tarefas",
+    'version': '10.0.1.0.0',
+    'category': "Taks",
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+        'Mackilem Van der Laan Soares <mack.vdl@gmail.com>'
+    ],
+    'depends': [
+        'base_report',
+        'project',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/task_materials.xml',
+    ],
+}
