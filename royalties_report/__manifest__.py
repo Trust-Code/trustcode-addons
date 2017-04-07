fd934653f0f1026066bf6b09806fe8eefc9db3c4
Fillipe Ramos <filliperamos90@gmail.com>
2017-04-07 16:24:24 -0300

    Royalties Invoice Report

---
+++ royalties_report/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2017 Fillipe Ramos, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Report de Comissões',
+    'description': "Report de Comissões",
+    'summary': "Report de Comissões",
+    'version': '10.0.1.0.0',
+    'category': "Report",
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+        'Mackilem Van der Laan Soares <mack.vdl@gmail.com>'
+    ],
+    'depends': [
+        'base_report',
+        'royalties',
+        'account',
+    ],
+    'data': [
+        'views/royalties_report.xml',
+    ],
+}
