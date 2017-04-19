efeda96ab057ee47cc19b054b32a3b3fb40faa5d
Mackilem <mack.vdl@gmail.com>
2017-04-19 17:04:15 -0300

    segunda parte do dev

--- purchase_agreements_kanban/models/purchase.py
+++ purchase_agreements_kanban/models/purchase.py
@@ -3,52 +3,79 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
-from datetime import datetime
-from dateutil.parser import parse
+from datetime import datetime, date
+from dateutil import parser
 
 class PurchaseAgreementsKanban(models.Model):
     _inherit = 'purchase.requisition'
 
-    sequence =fields.Integer(string="Kanban Sequece")
-    state_kanban = fields.Selection([
+    sequence=fields.Integer(store=True)
+    state_kanban=fields.Selection([
         	('req','Requisição de Compras'),
-            ('req_pdc','Requisição de Compras em PDC'),
-            ('req_concl','Requisição de Compras Conluída'),
+            ('req_pdc','Requisição em PDC'),
+            ('req_concl','Requisição Concluída'),
             ('req_fat','À Faturar'),
             ('req_fin','Finalizado'),
             ],
             string='Status Kanban',
+            compute='_get_state_kanban',
+            store=True,
             index=True,
             copy=False,
             default='req',
             track_visibility='onchange')
-
     image=fields.Many2one(
         compute="_get_image",
         comodel_name="ir.attachment",
         ondelete="set null")
+    days_end=fields.Integer(string="End Date in Days", store=True)
+    days_ordering=fields.Integer(string="Ordering in Days", store=True)
+    days_schedule=fields.Integer(string="Delivery in Days", store=True)
 
-    days_end=fields.Integer(string="End Date in Days", compute="_get_days")
-    days_ordering=fields.Integer(string="Ordering in Days")
-    days_delivery=fields.Integer(string="Delivery in Days")
+    @api.multi
+    def write(self,vals):
+        seq_dict = {
+            'req':['date_end','asc'],
+            'req_pdc':['ordering_date','asc'],
+            'req_concl':['schedule_date','desc'],
+            'req_fat':['schedule_date','asc'],
+            'req_fin':['schedule_date','desc'],
+            }
+        seq_col = seq_dict[self.state_kanban]
+        seq_ids = self.search(
+            [('state_kanban','in',[self.state_kanban])],
+            order=seq_col[0]+' '+seq_col[1],
+            limit=3)
+        for id in seq_ids:
+            if seq_col[1] == 'desc':
+                if self[seq_col[0]] < id[seq_col[0]]:
+                    vals['sequence']=1
+                    continue
+            else:
+                if self[seq_col[0]] > id[seq_col[0]]:
+                    vals['sequence']=1
+                    continue
+        return super(PurchaseAgreementsKanban,self).write(vals)
 
-    @api.depends('state_kanban')
-    def _get_sequence(self):
-        dict_seq = []
-            ['req','date_end asc',1000],
-            ['req_pdc','date_end asc',1000],
-            ]
-        for seq in dict_seq:
-            seq_state = self.search(
-                [('state_kanban','in',[seq[0]])], order=seq[1])
-
-            for rec in seq_state:
-                rec.sequence = seq[2]
-                seq[2] +=1
-
-
-
-            elif rec.state_kanban == 'req_pdc':
+    @api.depends('purchase_ids','order_count','state','purchase_ids.state')
+    def _get_state_kanban(self):
+        for rec in self:
+            purchase_ids = rec.env['purchase.order'].search(
+                                [('requisition_id','=',rec.id)])
+            if rec.state in ['draft','in_progress','open']:
+                rec.state_kanban = 'req'
+            if rec.state in ['draft','in_progress','open'] and purchase_ids:
+                rec.state_kanban = 'req_pdc'
+            if purchase_ids:
+                for id in purchase_ids:
+                    if id.state in ['purchase']:
+                        rec.state_kanban = 'req_fat'
+            if purchase_ids:
+                for id in purchase_ids:
+                    if id.state in ['done']:
+                        rec.state_kanban = 'req_fin'
+            if rec.state in ['done']:
+                rec.state_kanban = 'req_concl'
 
     @api.depends('account_analytic_id')
     def _get_image(self):
@@ -65,8 +92,14 @@ class PurchaseAgreementsKanban(models.Model):
             if attach_id:
                 rec.image=attach_id
 
-    @api.depends('date_end')
-    def _get_days(self):
-        for rec in self:
-            if rec.date_end:
-                rec.days_end=(datetime.now() - parse(rec.date_end)).days
+    @api.onchange('date_end','ordering_date','schedule_date')
+    def _onchange_days(self):
+        if self.date_end:
+            self.days_end=self._days(self.date_end)
+        if self.ordering_date:
+            self.days_ordering=self._days(self.ordering_date)
+        if self.schedule_date:
+            self.days_schedule=self._days(self.schedule_date)
+
+    def _days(self,date_from):
+        return ((parser.parse(date_from)).date() - date.today()).days
