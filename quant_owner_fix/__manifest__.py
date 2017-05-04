a38d46b09922a9646b8faec16c6a3ae00989b0fc
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-05-04 20:21:27 -0300

    Adiciona owner ao reservar o quant, se o picking possuir um owner setado

---
+++ quant_owner_fix/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2015 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': 'Trustcode - Fix Owner in Quants',
+    'description': 'Add Owner to quant when reserving',
+    'summary': """
+        Add Owner to quant when reserving
+    """,
+    'version': '10.0.1.0.0',
+    'category': 'Stock',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    'depends': [
+        'stock'
+    ],
+}
