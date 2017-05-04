aedf131efcbe216c311301f3441ee619fe4bb3c0
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-05-04 17:25:44 -0300

    Invertendo o campo de pedido de venda, filtrando por domain a linha

--- project_task_materials/models/project_task.py
+++ project_task_materials/models/project_task.py
@@ -70,8 +70,7 @@ class ProjectTaskMaterial(models.Model):
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    sale_order_id = fields.Many2one(
-        related='sale_line_id.order_id', string="Pedido", readonly=True)
+    sale_order_id = fields.Many2one('sale.order', string="Pedido")
     material_project_task_ids = fields.One2many(
         'project.task.material', 'task_id', string='Materiais Usados')
 
