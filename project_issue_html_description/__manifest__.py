f861fded745fc2949d003c68aa3422dbf566f667
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-05-09 14:59:16 -0300

    Módulo de descriçaõ em html nos issues

---
+++ project_issue_html_description/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Project Issue HTML Description',
+    'summary': 'Enhances the description field of issues.',
+    'author': 'Onestein',
+    'website': 'http://www.onestein.eu',
+    'category': 'Project Management',
+    'license': 'AGPL-3',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'project_issue',
+    ],
+    'installable': True,
+}
