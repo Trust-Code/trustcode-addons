15fb051126ad24fedbe41844ba048e277eec837d
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-06-27 16:26:18 -0300

    WIP - Migração dos módulos de configurar variantes do produto no pedido

---
+++ sale_product_variants/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2014-2016 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import models
+from .hooks import assign_product_template
---
+++ sale_product_variants/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# © 2014-2016 Oihane Crucelaegui - AvanzOSC
+# © 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Sale - Product variants",
+    "summary": "Product variants in sale management",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+        "sale",
+        "product_variants_no_automatic_creation",
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Tecnativa",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <ajuaristio@gmail.com>",
+    ],
+    "category": "Sales Management",
+    "website": "http://www.odoomrp.com",
+    "data": [
+        "views/sale_view.xml",
+    ],
+    "installable": True,
+}
