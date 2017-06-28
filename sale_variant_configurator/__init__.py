d1970336691922c4b861ed4385fa916cc093cd50
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-06-28 16:03:34 -0300

    Novo módulo simplificado que permite configurar o produto na linha do pedido de venda Ao selecionar os atributos a variante é selecionada automaticamente

--- sale_variant_configurator/__init__.py
+++ sale_variant_configurator/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Oihane Crucelaegui - AvanzOSC
+# © 2014-2016 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import test_product_variants
+from . import models
