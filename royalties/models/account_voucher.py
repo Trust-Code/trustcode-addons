0f58853dc69ffc6963b23457b64b679923cac4a0
Mackilem <mack.vdl@gmail.com>
2017-07-13 16:05:44 -0300

    Melhorias módulo de royalties - Criação de account voucher para pagamento de royalties

---
+++ royalties/models/account_voucher.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2017 Mackilem Van der Laan, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class AccountVoucher(models.Model):
+    _inherit = "account.voucher"
+
+    royalties_id = fields.Many2one('royalties',
+        string='Contrato Royalties',
+        ondelete='restrict',
+        domain="[('partner_id','=',partner_id),\
+                 ('state','=','in_progress')]")
+
+
+class AccountVoucherLine(models.Model):
+    _inherit = "account.voucher.line"
+
+    inv_line_id = fields.Many2one('account.invoice.line')
