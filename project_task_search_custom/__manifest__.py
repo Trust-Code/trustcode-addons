b8ee63c9abbe6227053d8e5d51b8a50218b1cca5
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-07-13 10:43:28 -0300

    Módulo para pesquisar o conteúdo da mensagem e observação em tarefas

---
+++ project_task_search_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Task Search Custom",
+    'summary': """Searching for tasks includes "Messages" and
+    "Description" fields""",
+    'author': "IT-Projects LLC, Ilmir Karamov",
+    'license': 'LGPL-3',
+    'website': "https://it-projects.info",
+    'version': '10.0.1.0.0',
+    'category': 'Project',
+    'images': ['images/task_search.png'],
+    'depends': ['project'],
+    'data': [
+        'views/project_task_search_custom.xml',
+    ],
+    'installable': True
+}
