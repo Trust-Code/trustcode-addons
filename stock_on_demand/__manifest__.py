64a4f120d98f5dfd49fa0d4530fa83876f67d0cc
Mackilem <mack.vdl@gmail.com>
2017-07-17 22:41:34 -0300

    Modulo para corrigir estoque

---
+++ stock_on_demand/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2017 Mackilem Van der Laan, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock on Demand',
+    'version': '1.0',
+    'category': 'Stock',
+    'sequence': 5,
+    'summary': 'New stock rule for especial case',
+    'description': """
+The
+====================================================
+
+""",
+    'website': 'https://www.trustcode.com.br',
+    'depends': ['stock',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
