90807e30de56c09eb7cc8473cf3c78cdb40273a9
Fábio Luna <fabiocluna@hotmail.com>
2017-07-21 16:19:42 -0300

    Criação da estrutura base do modulo

---
+++ royalties_report/Report/royalties_report.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+# © 2017 Fábio Luna, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class RoyaltiesReport(models.AbstractModel):
+    _name = 'report.royalties_report.report_royalties_main'
