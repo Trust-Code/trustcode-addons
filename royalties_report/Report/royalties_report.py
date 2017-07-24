9a0f8cca414a4ddfb0b4b6a2b2f91d0ce1a46bc5
Fábio Luna <fabiocluna@hotmail.com>
2017-07-24 10:23:17 -0300

    Implementção da classe de impressão do relatório

--- royalties_report/Report/royalties_report.py
+++ royalties_report/Report/royalties_report.py
@@ -7,3 +7,18 @@ from odoo import api, models
 
 class RoyaltiesReport(models.AbstractModel):
     _name = 'report.royalties_report.report_royalties_main'
+
+    @api.model
+    def render_html(self, docids, data=None):
+        report_obj = self.env['report']
+        report = report_obj._get_report_from_name(
+            'royalties_report.template_royalties_report_main')
+        royalties = self.env['royalties'].search([('id', 'in', docids)])
+
+        docargs = {
+            'doc_ids': docids,
+            'doc_model': report.model,
+            'docs': royalties,
+        }
+        return report_obj.render(
+            'royalties_report.template_royalties_report_main', docargs)
