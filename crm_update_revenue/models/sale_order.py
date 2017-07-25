867623d4db6e89144f786b6d3240a943d8a25372
Mackilem <mack.vdl@gmail.com>
2017-07-25 17:59:20 -0300

    atualizado a opção de soma que não estava dividindo entre recorrente e avulso, tornado o campo obrigatório e atualizado o nome do campo

--- crm_update_revenue/models/sale_order.py
+++ crm_update_revenue/models/sale_order.py
@@ -12,29 +12,29 @@ class SaleOrder(models.Model):
         ('1', 'Substituir'),
         ('2', 'Somar'),
         ('3', 'Não modificar')],
-        string='Receita Recorrente',
+        string='Atual. Oportunidade',
+        required=True,
         help="1 - Substitui o valor da oportunidade com o valor total desta "
         "cotação \n 2 - Soma os valores de todas as cotações vinculadas "
         "a oportunidade e sobrescreve o valor da mesma \n 3 - Não modifica "
-        "o valor da oportunidade")
+        "o valor da oportunidade",)
 
     @api.multi
     def write(self, vals):
         res = super(SaleOrder, self).write(vals)
-
         if self.update_option_recurrent_revenue == '1':
             for item in self:
-                item.opportunity_id.recurrent_revenue = item.amount_total
+                item.opportunity_id.planned_revenue = item.total_non_recurrent
+                item.opportunity_id.recurrent_revenue = item.total_recurrent
         elif self.update_option_recurrent_revenue == '2':
             cotacoes = self.search([
                 ('opportunity_id', '=', self.opportunity_id.id)])
+            total_recurrent = 0
+            total_non_recurrent = 0
+            for item in cotacoes:
+                total_recurrent += item.total_recurrent
+                total_non_recurrent += item.total_non_recurrent
 
-            total = 0
-
-            for cotacao in cotacoes:
-                total += cotacao.amount_total
-
-            for item in self:
-                item.opportunity_id.recurrent_revenue = total
-
+            item.opportunity_id.planned_revenue = total_non_recurrent
+            item.opportunity_id.recurrent_revenue = total_recurrent
         return res
