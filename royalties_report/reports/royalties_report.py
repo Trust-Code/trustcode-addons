45f3e0d0e49496bae8ed842a19d8cf739b100509
Fábio Luna <fabiocluna@hotmail.com>
2017-07-26 15:35:29 -0300

    implementação do período do relatório

--- royalties_report/reports/royalties_report.py
+++ royalties_report/reports/royalties_report.py
@@ -2,7 +2,8 @@
 # © 2017 Fábio Luna, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import api, models, fields
+import datetime
 
 
 class RoyaltiesReport(models.AbstractModel):
@@ -14,15 +15,37 @@ class RoyaltiesReport(models.AbstractModel):
         report = report_obj._get_report_from_name(
             'royalties_report.royalties_report')
         account_voucher = self.env['account.voucher'].search(
-                            [('id', 'in', docids)])
+            [('id', 'in', docids)])
         royalties_lines = self.env['account.royalties.line'].search(
-                            [('voucher_id', '=', account_voucher.id)])
+            [('voucher_id', '=', account_voucher.id)])
+
+        period = self._get_period(royalties_lines[0].royalties_id,
+                                  account_voucher)
+
+        initial_date = datetime.datetime.strptime(
+            period['initial_date'], '%Y-%m-%d').strftime('%d/%m/%Y')
+        final_date = datetime.datetime.strptime(
+            period['final_date'], '%Y-%m-%d').strftime('%d/%m/%Y')
 
         docargs = {
             'doc_ids': docids,
             'doc_model': report.model,
             'docs': account_voucher,
             'royalties_lines': royalties_lines,
+            'initial_date': initial_date,
+            'final_date': final_date
         }
         return report_obj.render(
             'royalties_report.royalties_report', docargs)
+
+    def _get_period(self, royalties_id, voucher_id):
+        account_voucher = self.env['account.voucher'].search(
+            [('royalties_id', '=', royalties_id.id),
+             ('id', '<=', voucher_id.id)], order='id desc', limit=1)
+
+        if not account_voucher or len(account_voucher) == 1:
+            return {'initial_date': royalties_id.start_date,
+                    'final_date': voucher_id.account_date}
+        else:
+            return {'initial_date': account_voucher[1].account_date,
+                    'final_date': account_voucher[0].account_date}
