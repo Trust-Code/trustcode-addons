84e1e669b7d2ed4632d1a8e4d8ffb110bcadc05f
Mackilem <mack.vdl@gmail.com>
2017-08-30 11:47:35 -0300

    módulo especial, pronto para o cliente

---
+++ issue_satisfaction_survey/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2017 Mackilem Van der Laan, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Issue Satisfaction Survey',
+    'version': '1.0',
+    'category': '',
+    'sequence': 5,
+    'summary': 'Pesquisa de satisfação em tarefas',
+    'description': """
+
+====================================================
+
+""",
+    'website': 'https://www.trustcode.com.br',
+    'depends': ['project_issue',],
+    'data': [
+        'wizard/project_issue.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
