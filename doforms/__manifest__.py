7d2e6bdddc1837915b178bd2c296b020968719bd
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-10-02 19:23:33 -0300

    Fix build related to flake

--- doforms/__manifest__.py
+++ doforms/__manifest__.py
@@ -4,7 +4,7 @@
 
 {   # pylint: disable=C8101,C8103
     'name': 'Formulário Base',
-    'description': "",
+    'description': "Formulário Base",
     'summary': "",
     'version': '10.0.1.0.0',
     'category': "",
@@ -16,12 +16,10 @@
         'Mackilem Van der Laan Soares <mack.vdl@gmail.com>'
     ],
     'depends': ['mail', 'project'],
-    'data': ['views/doforms.xml',
-             'data/ir_sequence.xml',
-             'security/security.xml',
-             'security/ir.model.access.csv'
-             ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-    }
+    'data': [
+        'views/doforms.xml',
+        'data/ir_sequence.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv'
+    ],
+}
--- doforms/models/doforms.py
+++ doforms/models/doforms.py
@@ -1,7 +1,8 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # © 2017 Mackilem Van der Laan, Trustcode
 # © 2017 Danimar Ribeiro, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 
 from odoo import api, fields, models
 
