94dddc60212890c74816eee9a27530ca9b87a12e
Mackilem <mack.vdl@gmail.com>
2017-10-02 09:18:37 -0300

    primeiro commit

---
+++ doforms/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2017 Mackilem Van der Laan, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'doforms',
+    'version': '1.0',
+    'category': '',
+    'sequence': 5,
+    'summary': '',
+    'description': """
+
+====================================================
+
+""",
+    'website': 'https://www.trustcode.com.br',
+    'depends': ['mail'],
+    'data': ['views/doforms.xml',
+             'data/ir_sequence.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
