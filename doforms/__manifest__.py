719737e84fdea0e0cd956e25dcdd8b4bcd7e2a78
Mackilem <mack.vdl@gmail.com>
2017-10-02 18:43:57 -0300

    erros de pep no manifest

--- doforms/__manifest__.py
+++ doforms/__manifest__.py
@@ -2,19 +2,19 @@
 # © 2017 Mackilem Van der Laan, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-{
-    'name': 'doforms',
-    'author': 'Mackilem Van der Laan',
-    'version': '1.0',
-    'category': '',
-    'sequence': 5,
-    'summary': '',
-    'description': """
-
-====================================================
-
-""",
-    'website': 'https://www.trustcode.com.br',
+{   # pylint: disable=C8101,C8103
+    'name': 'Formulário Base',
+    'description': "",
+    'summary': "",
+    'version': '10.0.1.0.0',
+    'category': "",
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+        'Mackilem Van der Laan Soares <mack.vdl@gmail.com>'
+    ],
     'depends': ['mail', 'project'],
     'data': ['views/doforms.xml',
              'data/ir_sequence.xml',
