ef2122b3707c47a005bd2fa18bc31efd44869a4e
Fábio Luna <fabiocluna@hotmail.com>
2017-10-18 11:40:56 -0200

    Exclui modulos que ainda não foram migrados para a V11

--- sale_order_contract/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 Danimar Ribeiro, Trustcode
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-
-from . import product
-from . import sale_order
-from . import payment_term
