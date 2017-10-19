2f7109eec9bf93a6e008413ad63b6dd563fc06bb
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-10-19 19:15:47 -0200

    Receber json de pedidos e compras via api

---
+++ stock_json_api/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2017 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Control Stock via API',
+    'description': "Control Stock via API",
+    'summary': "Control the incoming",
+    'version': '11.0.1.0.0',
+    'category': "stock",
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    'depends': [
+        'stock', 'purchase', 'sale'
+    ],
+    'data': [
+    ],
+}
