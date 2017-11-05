cecf2124ec7322b1b415714d38b4820a80619c9a
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-11-05 18:31:26 -0200

    Módulo para faturamento parcial por filiais

---
+++ sale_invoice_rules/models/sale_order.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# © 2017 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
