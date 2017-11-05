35ec41995c679a5a01aad583b0cc0a56b30a55bc
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-11-05 18:31:26 -0200

    Módulo para faturamento parcial por filiais

---
+++ sale_invoice_rules/models/res_partner.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2017 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    branch_id = fields.Many2one('res.partner', string="Matriz")
+    branch_ids = fields.One2many('res.partner', 'branch_id', string="Filiais")
+    company_invoice_id = fields.Many2one(
+        'res.company', string="Empresa Faturamento")
+    percentual_nota_debito = fields.Float(string="% Nota débito")
+    percentual_faturamento = fields.Float(string="Percentual Faturamento")
