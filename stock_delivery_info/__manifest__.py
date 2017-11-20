2c1578a17bba4143324a847be21a34ba61699e5e
Fábio Luna <fabiocluna@hotmail.com>
2017-11-20 17:36:42 -0200

    Inclui nova dependencia

--- stock_delivery_info/__manifest__.py
+++ stock_delivery_info/__manifest__.py
@@ -3,17 +3,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-{
+{  # pylint: disable=C8101,C8103
     'name': "stock_delivery_info",
 
     'summary': """
         Envia as informações de transporte, necessárias para a emissão
         de uma NF-e, com base no picking de entrega.""",
-
-    'description': """
-        Envia as informações de transporte, necessárias para a emissão
-        de uma NF-e, com base no picking de entrega.
-    """,
     'author': "Trustcode",
     'website': "http://www.trustcode.com.br",
     'category': 'Uncategorized',
@@ -25,6 +20,7 @@
     'depends': [
         'base',
         'delivery',
+        'stock_json_api',
     ],
     'data': [
         'views/stock_picking.xml',
