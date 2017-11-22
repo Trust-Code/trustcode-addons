83a015ea1a715736ba2899b5f5b0dbf5947c96c9
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-11-22 14:07:17 -0200

    Módulo para gerar varios projetos por pedido de venda

---
+++ project_per_sale_line/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2017 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{  # pylint: disable=C8101,C8103
+    'name': 'Projeto por Linha de Venda',
+    'description': 'Projeto por linha de venda',
+    'summary': """ Cria um projeto para cada linha do pedido de venda """,
+    'version': '11.0.1.0.0',
+    'category': 'Project',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+        'Mackilem Van der Laan Soares <mack.vdl@gmail.com>'
+    ],
+    'depends': [
+        'project', 'sale_timesheet'
+    ],
+    'data': [
+    ],
+}
