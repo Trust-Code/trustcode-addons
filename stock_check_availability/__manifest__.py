157b2bff9a8206a3530e249295ec63ab746fa847
carcaroff <johnychenjy@gmail.com>
2017-12-12 15:02:44 -0200

    [kinda FEAT]stock check availabilitygit status

---
+++ stock_check_availability/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2017 Johny Chen Jy, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': 'Trustcode - Manufacturing availability checker',
+    'description': 'Check stock availability for manufactured items',
+    'summary': """
+        It checks the availiability of the itens necessary for the production
+        of a manufactured item.
+    """,
+    'version': '10.0.1.0.0',
+    'category': 'Stock',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Johny Chen Jy <johnychenjy@gmail.com>',
+    ],
+    'depends': [
+        'sale_stock'
+    ],
+    'data': [],
+}
