2ceab091f1e9c2711288c6096aee5bcd7ab28111
carcaroff <johnychenjy@gmail.com>
2017-12-20 15:25:41 -0200

    agora vai

--- backup_edx/models/fabfile.py
+++ backup_edx/models/fabfile.py
@@ -32,18 +32,21 @@ def host_to_backup():
 # Método na qual executa as funções do backup
 def get_backup():
     # Faz um novo backup atraves do mongodump (Parte 1/2)
-    run('sudo docker exec trustcode-edx mongodump -d edxapp -o /edx/app/edxapp/edx-platform/backup')
+    run('sudo docker exec trustcode-edx mongodump -d edxapp -o \
+/edx/app/edxapp/edx-platform/backup')
     # Backup do mysql (Parte 2/2)
     run('sudo docker exec trustcode-edx mysqldump -u root edxapp > edx.sql')
     # Transfere o folder do docker para a maquina remota
-    run('sudo docker cp trustcode-edx:/edx/app/edxapp/edx-platform/backup ~/backup')
+    run('sudo docker cp trustcode-edx:/edx/app/edxapp/edx-platform/backup \
+~/backup')
     # Comprime a pasta backup num arquivo tar.gz
     run('tar -czvf backup.tar.gz edx.sql backup')
     # Transfere o arquivo zip da maquina remota para a maquina local
     get('~/backup.tar.gz', '~/backup.tar.gz')
 
     # Remove todos os arquivos remanescentes
-    run('sudo docker exec trustcode-edx rm -R /edx/app/edxapp/edx-platform/backup')
+    run('sudo docker exec trustcode-edx rm -R /edx/app/edxapp/edx-platform/\
+backup')
     run('sudo rm -r backup')
     run('sudo rm -f edx.sql')
     run('sudo rm -f backup.tar.gz')
@@ -62,14 +65,18 @@ def restore_backup():
     # Descompacta o arquivo transferido
     run('tar -vzxf backup.tar.gz')
     # Transfere o arquivo do backup da maquina virtual para o docker
-    run('sudo docker cp /home/ubuntu/backup trustcode-edx:/edx/app/edxapp/edx-platform/backup')
+    run('sudo docker cp /home/ubuntu/backup trustcode-edx:/edx/app/edxapp/\
+edx-platform/backup')
     # Faz o restore atraves do mongorestore (1/2)
-    run('sudo docker exec trustcode-edx mongorestore --drop /edx/app/edxapp/edx-platform/backup')
+    run('sudo docker exec trustcode-edx mongorestore --drop /edx/app/edxapp/\
+edx-platform/backup')
     # Faz restore do mysql (2/2)
-    run('sudo docker exec -i trustcode-edx /usr/bin/mysql -u root edxapp < edx.sql')
+    run('sudo docker exec -i trustcode-edx /usr/bin/mysql -u root edxapp < \
+edx.sql')
 
     # Deleta os arquivos remanescentes
-    run('sudo docker exec trustcode-edx rm -R /edx/app/edxapp/edx-platform/backup')
+    run('sudo docker exec trustcode-edx rm -R /edx/app/edxapp/edx-platform/\
+backup')
     run('sudo rm -r edx.sql')
     run('sudo rm -r backup')
     run('sudo rm -f backup.tar.gz')
