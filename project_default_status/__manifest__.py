68c3fa1b5266814058913d2c884586a3bf5286cb
Felipe <paloschi.eca@gmail.com>
2018-01-10 11:27:43 -0200

    criação campo default, ao criar projeto, vem com estágios default

---
+++ project_default_status/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2017 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{  # pylint: disable=C8101,C8103
+    'name': "Estágio Padrão para Projetos",
+
+    'summary': """
+        Define um modelo padrão para o estágio dos projetos""",
+
+    'description': """
+    """,
+    'author': "Trustcode",
+    'website': "http://www.trustcode.com.br",
+    'category': 'Uncategorized',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'contributors': [
+
+    ],
+    'depends': ['project'],
+    'data': [
+        'views/project_task_type.xml'
+    ],
+}
