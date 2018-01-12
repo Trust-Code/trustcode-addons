d2a8f6e6657c8606fa4405db289955895695f97c
Fábio Luna <fabiocluna@hotmail.com>
2018-01-12 17:51:09 -0200

    Cria estrutura do módulo.

---
+++ edx_integration/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2018 Fábio Luna
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'EDX Integration',
+    'description': "Módulo para integrar Odoo ao Open EDX",
+    'version': '11.0.1.0.0',
+    'category': "Administration",
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Fábio Luna <fabiocluna@hotmail.com>',
+    ],
+    'depends': [
+        'br_base',
+    ],
+}
