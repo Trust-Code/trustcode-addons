e7da7cda88813f2cd186c86747d37b237e2d2125
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-01-25 16:26:53 -0200

    Revertendo mudanças no módulo de satisfação

--- issue_satisfaction_survey/models/project_issue.py
+++ issue_satisfaction_survey/models/project_issue.py
@@ -21,11 +21,10 @@ class ProjectIssue(models.Model):
     pergunta3 = fields.Selection([('otimo', u'Ótimo'), ('bom', u'Bom'),
                                   ('regular', u'Regular'), ('ruim', u'Ruim'),
                                   ('pessimo', u'Péssimo')])
-    pergunta4 = fields.Selection([('1', '1 - Muito Instatisfeito'),
-                                  ('2', '2 - Insatisfeito'),
-                                  ('3', '3 - Indiferente'),
-                                  ('4', '4 - Satisfeito'),
-                                  ('5', '5 - Muito Satisfeito')])
+    pergunta4 = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'),
+                                  ('4', '4'), ('5', '5'), ('6', '6'),
+                                  ('7', '7'), ('8', '8'), ('9', '9'),
+                                  ('10', '10')])
 
     @api.multi
     def write(self, vals):
