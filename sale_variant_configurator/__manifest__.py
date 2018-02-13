912fcd22089bea6952d5851a1a650197ea5436f3
Felipe <paloschi.eca@gmail.com>
2018-02-13 15:38:58 -0200

    finalização de metodos e views sale_config_variant

--- sale_variant_configurator/__manifest__.py
+++ sale_variant_configurator/__manifest__.py
@@ -2,26 +2,23 @@
 # © 2018 Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-
 {  # pylint: disable=C8101,C8103
     'name': "sale_variant_configurator",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Permite selecionar nas linhas da cotação o as variações do produto.""",
 
-    'description': """
-        Long description of module's purpose
-    """,
+    'description': """""",
     'author': "Trustcode",
     'website': "http://www.trustcode.com.br",
-    'category': 'Uncategorized',
+    'category': 'Sale',
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
-    'contributors': [],
+    'contributors': [
+        'Mackilem Van der Laan Soares <mack.vdl@gmail.com>',
+        'Felipe Paloschi <paloschi.eca@gmail.com>'],
     'depends': ['sale'],
     'data': [
         'views/sale_order.xml',
-        'wizard/wizard_sale_variant.xml'
     ],
 }
