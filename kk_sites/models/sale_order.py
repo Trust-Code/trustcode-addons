7a9e4df6319aa4786ba8ebb556f0ba60777a508b
Felipe <paloschi.eca@gmail.com>
2018-03-02 18:21:11 -0300

    correções servidor kk

--- kk_sites/models/sale_order.py
+++ kk_sites/models/sale_order.py
@@ -15,23 +15,41 @@ class SaleOrderLine(models.Model):
 
     description_proposta = fields.Html(string="Descrição para proposta")
 
+    def get_next_folder_number(self, task):
+        link = task.kk_site_id.pasta_servidor
+        res = self.env['kk.sites'].get_server_folders(link, False)
+        numbers = []
+        for item in res['folders']:
+            numbers.append(item['name'][:2])
+        for index in range(len(numbers)):
+            try:
+                numbers[index] = int(numbers[index])
+            except ValueError:
+                numbers[index] = 0
+        numbers.sort()
+        return numbers[-1]
+
     def _create_server_service_dir(self, res):
         task = res[self.id]
         access_token = self.env.user.company_id.egnyte_acess_token
         host = self.env.user.company_id.egnyte_host
         headers = {'Authorization': 'Bearer ' + access_token,
                    'Content-Type': 'application/json'}
+        number = self.get_next_folder_number(task)
         pasta = task.kk_site_id.pasta_servidor.replace(
             'https://' + host + '.egnyte.com/app/index.do#storage/files/1',
             '').replace('%20', ' ')
-        name = task.name.replace(':', '_').strip()
-        pasta += '/' + normalize('NFKD', name).encode('ASCII', 'ignore').\
-            decode('ASCII').upper()
+        name = str(number + 1).zfill(2) + '_' + task.name.replace(
+            ':', '_').strip()
+        pasta += '/' + normalize(
+            'NFKD', name).encode('ASCII', 'ignore').decode('ASCII').upper()
         domain = 'https://' + host + '.egnyte.com/pubapi/v1/fs' + pasta
         data = {"action": "add_folder"}
         data = json.dumps(data)
         response = requests.post(domain, headers=headers, data=data)
         self.env['kk.sites'].parse_response(response)
+        self.order_id.message_post('Criada pasta de serviço no servidor: %s'
+                                   % pasta)
 
     @api.multi
     def _timesheet_find_task(self):
@@ -41,7 +59,8 @@ class SaleOrderLine(models.Model):
             task.write({'kk_site_id': so_line.kk_site_id.id,
                         'name': '%s:%s' % (so_line.order_id.name,
                                            so_line.product_id.name)})
-            self._create_server_service_dir(result)
+            if so_line.kk_site_id:
+                self._create_server_service_dir(result)
         return result
 
     @api.onchange('product_id')
