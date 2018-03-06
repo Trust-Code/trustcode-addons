61c5dfcd878aad6af9ea7d2610b17dc19bbf2e07
Felipe <paloschi.eca@gmail.com>
2018-03-06 15:28:09 -0300

    bug ccm account voucher

---
+++ ccm_account_voucher_edit/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2017 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': "Edição Account Voucher CCM",
+
+    'summary': """
+        Correção de bug ao tentar setar manualmente a conta
+        em uma linha do Recebimento de Compra""",
+
+    'description': """""",
+    'author': "Trustcode",
+    'website': "http://www.trustcode.com.br",
+    'category': 'Accounting & Finance',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'contributors': [
+        'Felipe Paloschi <paloschi.eca@gmail.com>',
+    ],
+    'depends': ['account'],
+    'data': [],
+}
