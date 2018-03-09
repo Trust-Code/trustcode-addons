af773420b1a727edde8fa56e01e2bfb78b13ed96
Felipe <paloschi.eca@gmail.com>
2018-03-09 17:58:11 -0300

    criacao de campos fluxo de compras

---
+++ gml_fluxo_compras/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2018 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': "Fluxo de Compras GML",
+
+    'summary': """
+        Ajuste do fluxo de Compras para empresa GML""",
+
+    'description': """""",
+    'author': "Trustcode",
+    'website': "http://www.trustcode.com.br",
+    'category': 'Purchase',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'contributors': [
+        'Felipe Paloschi <paloschi.eca@gmail.com>',
+    ],
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order.xml',
+    ],
+}
