7187e51c9b80013098103be37181859a2fa394c8
Felipe <paloschi.eca@gmail.com>
2018-03-13 13:56:25 -0300

    criação de fluxo de vendas design

---
+++ sale_design/models/product.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# © 2018 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    allow_design = fields.Boolean('Permitir Design')
