1cc68c9c8ed14d7f849894edf3ebe34f5b6d3fd0
Felipe <paloschi.eca@gmail.com>
2018-04-02 17:07:24 -0300

    correcao campos kk_sites

--- kk_sites/models/project.py
+++ kk_sites/models/project.py
@@ -14,10 +14,22 @@ class Project(models.Model):
         store=True)
     art = fields.Char('ART')
     qualidade = fields.Char('Qualidade')
-    data_entrega = fields.Date('Data Previsão Entrega')
+    data_entrega = fields.Date(
+        'Data Previsão Entrega',
+        compute='_compute_data_entrega')
     ultima_alteracao = fields.Datetime('Última Alteração')
-    arquivado_fisicamente = fields.Char('Arquivado Fisicamente Em')
+    arquivado_fisicamente = fields.Date('Arquivado Fisicamente Em')
     obs = fields.Html('Observação')
+
+    @api.multi
+    def _compute_data_entrega(self):
+        for project in self:
+            data_entrega = project.task_ids.search(
+                [('date_deadline', '!=', False),
+                 ('project_id', '=', project.id)],
+                order="date_deadline desc", limit=1).date_deadline
+            if data_entrega:
+                project.update({'data_entrega': data_entrega})
 
 
 class Task(models.Model):
@@ -30,6 +42,8 @@ class Task(models.Model):
 
     @api.model
     def create(self, vals):
+        project = self.env['project.project'].browse(vals['project_id'])
+        if not vals.get('kk_site_id'):
+            vals.update({'kk_site_id': project.kk_site_id.id})
         res = super(Task, self).create(vals)
-        res.kk_site_id = res.project_id.kk_site_id
         return res
