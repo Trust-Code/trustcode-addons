8150417a0ad0317706f13fffbc0034d300122cf8
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-04-13 15:56:28 -0300

    Módulo inicial para gerenciamento de compras entre filiais

---
+++ multicompany_purchase_requisition/__manifest__.py
@@ -0,0 +1,26 @@
+# © 2018 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{   # pylint: disable=C8101,C8103
+    'name': 'Requisição de compras - Multi-empresas',
+    'summary': """Requisição de compras - Multi-empresas""",
+    'version': '11.0.1.0.0',
+    'category': 'Extra Tools',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    "description": """
+        Requisição de compras - Multi-empresas
+    """,
+    'depends': [
+        'purchase_requisition',
+    ],
+    'data': [
+        "data/multicompany_purchase.xml",
+        "views/purchase_multicompany.xml",
+    ],
+}
---
+++ multicompany_purchase_requisition/models/purchase_multicompany.py
@@ -0,0 +1,87 @@
+# © 2018 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from datetime import datetime
+from odoo import api, fields, models, _
+from odoo.addons import decimal_precision as dp
+from odoo.exceptions import UserError
+
+
+class PurchaseMulticompany(models.Model):
+    _name = "purchase.multicompany"
+    _description = "Purchase Requisition for Multi Company"
+    _inherit = ['mail.thread']
+    _order = "id desc"
+
+    name = fields.Char(
+        string='Agreement Reference', required=True, copy=False,
+        default=lambda self: self.env['ir.sequence'].next_by_code(
+            'purchase.multicompany.requisition'))
+    ordering_date = fields.Date(string="Ordering Date")
+    date_end = fields.Datetime(string='Agreement Deadline')
+    schedule_date = fields.Date(string='Delivery Date', index=True)
+    user_id = fields.Many2one(
+        'res.users', string='Responsible', default=lambda self: self.env.user)
+    description = fields.Text()
+    company_id = fields.Many2one(
+        'res.company', string='Company', required=True,
+        default=lambda self: self.env['res.company']._company_default_get(
+            'purchase.requisition'))
+    line_ids = fields.One2many(
+        'purchase.multicompany.line', 'requisition_id',
+        string='Products to Purchase', states={'done': [('readonly', True)]},
+        copy=True)
+    state = fields.Selection(
+        [('draft', 'Draft'), ('in_progress', 'Confirmed'),
+         ('needs_attention', 'Needs attention'),
+         ('done', 'Done'), ('cancel', 'Cancelled')],
+        'Status', track_visibility='onchange', required=True,
+        copy=False, default='draft')
+
+    @api.multi
+    def action_cancel(self):
+        self.write({'state': 'cancel'})
+
+    @api.multi
+    def action_in_progress(self):
+        if not all(obj.line_ids for obj in self):
+            raise UserError(
+                _('You cannot confirm call because there is no product line.'))
+        self.write({'state': 'in_progress', 'ordering_date': datetime.now()})
+
+    @api.multi
+    def action_draft(self):
+        self.write({'state': 'draft'})
+
+    @api.multi
+    def action_done(self):
+        self.write({'state': 'done'})
+
+
+class PurchaseMulticompanyLine(models.Model):
+    _name = "purchase.multicompany.line"
+    _description = "Purchase MultiCompany Line"
+    _rec_name = 'product_id'
+
+    product_id = fields.Many2one(
+        'product.product', string='Product',
+        domain=[('purchase_ok', '=', True)], required=True)
+    product_uom_id = fields.Many2one(
+        'product.uom', string='Product Unit of Measure')
+    product_qty = fields.Float(
+        string='Quantity', digits=dp.get_precision('Product Unit of Measure'))
+    qty_increment = fields.Float(
+        string='Increment', digits=dp.get_precision('Product Unit of Measure'))
+    requisition_id = fields.Many2one(
+        'purchase.multicompany', string='Purchase Company', ondelete='cascade')
+    company_id = fields.Many2one(
+        'res.company', related='requisition_id.company_id', string='Company',
+        store=True, readonly=True,
+        default=lambda self: self.env['res.company']._company_default_get(
+            'purchase.multicompany.line'))
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        if self.product_id:
+            self.product_uom_id = self.product_id.uom_id
+            self.product_qty = 1.0
