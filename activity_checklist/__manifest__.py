b6c57038855a4b880e25cfafaeee747815fc0328
Felipe <paloschi.eca@gmail.com>
2018-04-30 17:56:25 -0300

    criacao de modelo checklist

---
+++ activity_checklist/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2018 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': "Checklist de Atividades",
+
+    'summary': """
+        Módulo para a criação de um checklist para as atividades""",
+
+    'description': """""",
+    'author': "Trustcode",
+    'website': "http://www.trustcode.com.br",
+    'category': 'Uncategorized',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'contributors': [
+        'Felipe Paloschi <paloschi.eca@gmail.com>',
+    ],
+    'depends': ['mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/mail_activity.xml',
+        'views/mail_activity_checklist.xml',
+    ],
+    'qweb': ['static/src/xml/*.xml'],
+}
---
+++ activity_checklist/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2018 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import mail_activity_checklist
+from . import mail_activity
