6bcc9308c9724bafbd7300b6e2038eb56eacb127
Felipe <paloschi.eca@gmail.com>
2018-05-04 13:03:39 -0300

    adicionado estilos ao módulo de checklist

--- activity_checklist/models/project.py
+++ activity_checklist/models/project.py
@@ -2,7 +2,7 @@
 # © 2018 Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models, fields
+from odoo import models, fields
 
 
 class Task(models.Model):
