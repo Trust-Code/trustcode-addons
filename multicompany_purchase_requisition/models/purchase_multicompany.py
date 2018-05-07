f01680ee8115f163e2c39189f73b8f977ca3b397
carcaroff <johnychenjy@gmail.com>
2018-05-07 17:14:57 -0300

    Adicionando alguns comentarios para facilitar futuro entendimento do codigo

--- multicompany_purchase_requisition/models/purchase_multicompany.py
+++ multicompany_purchase_requisition/models/purchase_multicompany.py
@@ -58,7 +58,8 @@ Try cancelling the Assembled Multicompany Purchase object first.')
     def action_in_progress(self):
         if not all(obj.line_ids for obj in self):
             raise UserError(
-                _('You cannot confirm call because there is no product line.'))
+                _(u'You cannot confirm call because there is no product line.'
+                  ))
         self.write({'state': 'in_progress', 'ordering_date': datetime.now()})
 
     @api.multi
@@ -85,11 +86,11 @@ Try cancelling the Assembled Multicompany Purchase object first.')
         centralizador_id = self.env[
             'purchase.multicompany.req'].create(vals)
         if not all(item.state == 'in_progress' for item in self):
-            raise UserError(_(
-                u"It's not possible to assemble non confirmed purchase \
+            raise UserError(
+                _(u"It's not possible to assemble non confirmed purchase \
 requisitions. Please, confirm those purchases or assemble the non \
 confirmed purchases later."
-            ))
+                  ))
         for item in self:
             item.centralizador_id = centralizador_id.id
         centralizador_id.assemble_selected(list(self))
