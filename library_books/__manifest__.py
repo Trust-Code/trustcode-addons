1ee20c7d451cc2b2720f11e803c3a619ce4bfd50
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-05-18 18:54:37 -0300

    [PEP] Fix pep issues in the code

--- library_books/__manifest__.py
+++ library_books/__manifest__.py
@@ -1,11 +1,11 @@
 # © 2016 Wellysson Melo, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-{
+{   # pylint: disable=C8101,C8103
     'name': 'Library Books',
     'summary': """Module to register books""",
     'description': "Module to register books",
-    'version': '11.0',
+    'version': '11.0.1.0.0',
     'category': 'Tools',
     'author': 'Trustcode',
     'license': 'AGPL-3',
--- library_books/models/library_books.py
+++ library_books/models/library_books.py
@@ -1,7 +1,7 @@
 # © 2016 Wellysson Melo, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class LibraryBooks(models.Model):
@@ -9,13 +9,18 @@ class LibraryBooks(models.Model):
 
     name = fields.Char('Title', size=150, required='1')
     author_id = fields.Many2one('res.partner', string='Author')
-    publishing_company_id = fields.Many2one('res.partner', string='Publishing Company', required='1')
-    original_publishing_company = fields.Char('Publishing Company(Orig. Name)', size=150, help="publishing company's original name")
-    edition = fields.Char('Edition', help="Edition Number", size=2, required='1')
+    publishing_company_id = fields.Many2one(
+        'res.partner', string='Publishing Company', required='1')
+    original_publishing_company = fields.Char(
+        'Publishing Company(Orig. Name)', size=150,
+        help="Publishing company's original name")
+    edition = fields.Char(
+        'Edition', help="Edition Number", size=2, required='1')
     release_year = fields.Integer('Release Year')
     chapters = fields.Integer('Chapters')
     pages = fields.Integer('Pages')
-    original_publication_id = fields.Many2one('library.books.publication.type', string='Original Pub.')
+    original_publication_id = fields.Many2one(
+        'library.books.publication.type', string='Original Pub.')
     doi = fields.Char('DOI', size=150, help="Digital Object Identifier")
     request_date_doi = fields.Date('Request Date DOI')
     license_id = fields.Many2one('library.books.license', string='License')
@@ -26,25 +31,34 @@ class LibraryBooks(models.Model):
     eisbn = fields.Char('eISBN', size=20)
     value_usd = fields.Float('USD', help="United States Dollar")
     value_br = fields.Float('R$', help="Real (Brazilian Coin)")
-    access_type_id = fields.Many2one('library.books.access.type', string='Access')
-    series_collection_id = fields.Many2one('library.books.series.collection', string='Series/Colection')
+    access_type_id = fields.Many2one(
+        'library.books.access.type', string='Access')
+    series_collection_id = fields.Many2one(
+        'library.books.series.collection', string='Series/Colection')
     with_panding = fields.Boolean('With Panding')
     has_parts = fields.Boolean('Has Parts?')
     is_published = fields.Boolean('Published')
     publication_date = fields.Date('Publication Date')
     comments = fields.Text('Comments')
-    feedback_type_id = fields.Many2one('library.books.feedback.type', string='Feedback Type')
-    files_format_id = fields.Many2one('library.books.files.format', string='Files Format')
+    feedback_type_id = fields.Many2one(
+        'library.books.feedback.type', string='Feedback Type')
+    files_format_id = fields.Many2one(
+        'library.books.files.format', string='Files Format')
     has_printed = fields.Boolean('Has Printed')
     is_digitized = fields.Boolean('Digitized')
     is_backoffice = fields.Boolean('Backoffice')
-    pdf_split = fields.Selection([('not_applicable', 'Not Applicable'), ('ok', 'OK'), ('produce', 'Produce')], string='PDF Split')
-    cover_pages = fields.Selection([('ok', 'OK'), ('produce', 'Produce')], string='Cover Pages')
+    pdf_split = fields.Selection(
+        [('not_applicable', 'Not Applicable'), ('ok', 'OK'),
+         ('produce', 'Produce')], string='PDF Split')
+    cover_pages = fields.Selection(
+        [('ok', 'OK'), ('produce', 'Produce')], string='Cover Pages')
     is_epub_published = fields.Boolean('ePUB Published')
     epub_version = fields.Boolean('ePUB Version')
     provider_id = fields.Many2one('res.partner', string='Provider')
-    value_usd_production = fields.Float('USD', help="Production Cost(USD - United States Dollar)")
-    value_br_production = fields.Float('R$', help="Production Cost (Real - Brazilian Coin)")
+    value_usd_production = fields.Float(
+        'USD', help="Production Cost(USD - United States Dollar)")
+    value_br_production = fields.Float(
+        'R$', help="Production Cost (Real - Brazilian Coin)")
     eisbn_order_lot = fields.Char('eISBN Order Lot')
     send_date = fields.Date('Send Date')
     prevision_date = fields.Date('Prevision Date')
@@ -89,7 +103,8 @@ class LibraryBooks(models.Model):
     oclc_update_date = fields.Date('Update Date')
     kbart_file = fields.Binary('KBART')
     kbart_filename = fields.Char('KBART file name')
-    responsible_provider = fields.Many2one('res.partner', string="Responsible Provider for Marking")
+    responsible_provider = fields.Many2one(
+        'res.partner', string="Responsible Provider for Marking")
     markup_lot = fields.Char('Markup Lot')
     volume = fields.Char('Volume')
     references_number = fields.Integer('References Number')
@@ -102,38 +117,44 @@ class LibraryBooks(models.Model):
     bisac = fields.Char('BISAC', size=150)
     bisac_complem = fields.Char('BISAC Complement', size=150)
 
+
 class LibraryBooksMeeting(models.Model):
     _name = 'library.books.meeting'
 
     name = fields.Char('Name', size=50, required='1')
+
 
 class LibraryBooksPublicationType(models.Model):
     _name = 'library.books.publication.type'
 
     name = fields.Char('Name', size=50, required='1')
 
+
 class LibraryBooksLicense(models.Model):
     _name = 'library.books.license'
 
     name = fields.Char('Name', size=150, required='1')
+
 
 class LibraryBooksAccessType(models.Model):
     _name = 'library.books.access.type'
 
     name = fields.Char('Name', size=50, required='1')
 
+
 class LibraryBooksSeriesCollection(models.Model):
     _name = 'library.books.series.collection'
 
     name = fields.Char('Name', size=150, required='1')
+
 
 class LibraryBooksFeedbackType(models.Model):
     _name = 'library.books.feedback.type'
 
     name = fields.Char('Name', size=50, required='1')
 
+
 class LibraryBooksFilesFormat(models.Model):
     _name = 'library.books.files.format'
 
     name = fields.Char('Name', size=50, required='1')
-
