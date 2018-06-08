291e7e12df300dadd87a2f4bfce6bf6455e5d3a4
Felipe <paloschi.eca@gmail.com>
2018-06-08 12:46:20 -0300

    Module OTRS Integration

---
+++ otrs_integration/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2018 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{   # pylint: disable=C8101,C8103
+    'name': "otrs_integration",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "Trustcode",
+    'website': "http://www.trustcode.com.br",
+    'category': 'Uncategorized',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'contributors': [
+        
+    ],
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+}
