fee85927a0698569f38ebacc69a805aecbb60eef
carcaroff <johnychenjy@gmail.com>
2018-07-12 16:31:08 -0300

    [11.0]commitment and requisition dates

---
+++ commitment_requisition_date/models/mrp_production.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2018 Johny Chen Jy, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+
+    commitment_date = fields.Datetime('Data do Compromisso')
+    requisition_date = fields.Datetime('Data do Requisição')
