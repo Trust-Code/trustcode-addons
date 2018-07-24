123ae163da27090107e5068d6258ea8357477c38
mackilem <mack.vdl@gmail.com>
2018-07-24 18:21:23 -0300

    first commit

---
+++ purchase_kits/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2017 Mackilem Van der Laan, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Purchase Order Kits',
+    'version': '1.0',
+    'category': 'Trustcode',
+    'sequence': 5,
+    'author': 'Trustcode',
+    'summary': 'Purchase order with kits',
+    'description': """
+""",
+    'website': 'https://www.trustcode.com.br',
+    'contributors': ['Mackilem Van der Laan <mack.vdl@gmail.com>'],
+    'depends': ['purchase'],
+    'data': ['views/purchase_order.xml'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ purchase_kits/models/purchase_kit.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# © 2017 Mackilem Van der Laan, Trustcode
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    is_kit = fields.Boolean(string=u"Is Kit?")
+    kit_order_id = fields.Many2one(
+        string="Purchase Order",
+        comodel_name="purchase.order",
+        ondelete="set null",
+    )
+    default_order_id = fields.Many2one(
+        string="Purchase Order",
+        comodel_name="purchase.order",
+        ondelete="set null",
+    )
+    order_id = fields.Many2one(required=0)
+
+    @api.model
+    def create(self, vals):
+        if 'default_order_id' in vals:
+            vals['order_id'] = vals['default_order_id']
+        elif 'kit_order_id' in vals:
+            vals['order_id'] = vals['kit_order_id']
+        super(PurchaseOrderLine, self).create(vals)
+
+    @api.multi
+    def _create_stock_moves(self, picking):
+        moves = self.env['stock.move']
+        done = self.env['stock.move'].browse()
+        is_kit = True if self.order_id.is_kit else False
+        for line in self.search([('order_id', '=', self.order_id.id),
+                                ('is_kit', '=', is_kit)]):
+            for val in line._prepare_stock_moves(picking):
+                done += moves.create(val)
+        return done
