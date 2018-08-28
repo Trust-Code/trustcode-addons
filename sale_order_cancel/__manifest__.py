06e0423e9b568d4cb5a1d0e073f627c167195e7a
carcaroff <johnychenjy@gmail.com>
2018-08-28 16:03:07 -0300

    Fix lynt: 1ts attempt

--- sale_order_cancel/__manifest__.py
+++ sale_order_cancel/__manifest__.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
+# © 2018 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {  # pylint: disable=C8101,C8103
     'name': "sale_order_cancel",
-
     'summary': """
         Cancela as ordens de venda ao cancelar um pedido
         - Mantido por Trustcode""",
-
     'description': """
         Cancela as ordens de venda ao cancelar um pedido
         - Mantido por Trustcode""",
-
     'author': "Trustcode",
     'website': "http://www.trustcode.com.br",
     'category': 'Uncategorized',
-    'version': '11.0.0.1',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'contributors': [
         'Guilherme Lenon da Silva <guilhermelds@gmail.com>',
     ],
     'depends': [
         'base',
-        'sale'
+        'sale',
     ],
 }
