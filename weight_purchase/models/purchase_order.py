68d4740a385a1b6c5d0d92858b9118ed0ac080fb
marinaGD <mgd.marinadomingues@gmail.com>
2018-08-28 16:56:35 -0300

    fix _ and when any product is added

--- weight_purchase/models/purchase_order.py
+++ weight_purchase/models/purchase_order.py
@@ -8,9 +8,13 @@ from odoo.exceptions import UserError
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    @api.onchange('total_frete', 'product_id')
+    @api.onchange('total_despesas', 'total_seguro', 'product_id',
+                  'total_frete', 'total_despesas_aduana')
     def _onchange_frete(self):
         super(PurchaseOrder, self)._onchange_despesas_frete_seguro()
+        if self.total_frete == 0 or (
+                not self.fiscal_position_id.fiscal_type == 'import'):
+            return
         full_weight = self._calc_total_weight()
         res = {}
         for line in self.order_line:
@@ -20,11 +24,14 @@ class PurchaseOrder(models.Model):
                 'valor_frete': valor_frete
             })
             if valor_frete == 0:
-                res = {'warning': {
+                res = {
+                    'warning': {
                         'title': _('Warning'),
                         'message': _("O produto %s tem peso igual a zero, \
                             caso não seja alterado, o rateio do frete \
-                            não o considerará." % (line.product_id.name))}}
+                            não o considerará.") % (line.product_id.name),
+                    }
+                }
         if 'warning' in res:
             return res
 
@@ -40,7 +47,7 @@ class PurchaseOrder(models.Model):
         for line in self.order_line:
             if line.product_id.fiscal_type == 'product':
                 full_weight += (line.product_id.weight * line.product_qty)
-        if full_weight == 0:
+        if full_weight == 0 and self.order_line:
             raise UserError(_("Nenhum dos produtos possui peso cadastrado. \
                     É necessário corrigir para o calculo do frete."))
         return full_weight
