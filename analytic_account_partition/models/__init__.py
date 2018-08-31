2376fcfc571b973edc4b50f3ad1659632c06c476
mackilem <mack.vdl@gmail.com>
2018-08-31 16:24:07 -0300

    removido os módulos de rateio para reforma!

--- analytic_account_partition/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2018 Trustcode
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import analytic_partition
-from . import analytic_account
-from . import account_move
-from . import account
