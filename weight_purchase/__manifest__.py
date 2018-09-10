d4ff995407508a2404d4e7ad4e6dcac978885c4d
marinaGD <mgd.marinadomingues@gmail.com>
2018-09-10 12:34:16 -0300

    fix flase8

--- weight_purchase/__manifest__.py
+++ weight_purchase/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Trustcode - Change tax calculation',
     'summary': """
         Change tax calculation to ZK Teco solicitation, and IN SRF 237/09 """,
-    'description': 'Change tax calculation',
+    'description': """Change tax calculation""",
     'version': '11.0.1.0.0',
     'category': 'Tax',
     'author': 'Trustcode',
