2c2608e621e461cd3e3c8d9575ab2f95ca2e84e4
Felipe Paloschi <paloschi.eca@gmail.com>
2018-09-18 13:26:59 -0300

    Add module group_quotations

---
+++ group_quotations/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{  # pylint: disable=C8101,C8103
+    'name': "Agrupar Cotações",
+
+    'summary': """Permite o agrupamento de cotações que contenham
+    mesma empresa, parceiro e posição fiscal.
+    """,
+
+    'description': """""",
+
+    'category': 'Sales',
+    'version': '0.1',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Felipe Paloschi <paloschi.eca@gmail.com>',
+    ],
+    'depends': ['sale'],
+
+    'data': [
+        'views/sale_order.xml',
+    ],
+}
