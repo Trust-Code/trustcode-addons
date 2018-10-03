6dfefa13b9a7a3fab652af7e74296fb3f505b416
Augusto D. Lisbôa <augusto.diniz.l@gmail.com>
2018-10-03 09:15:37 -0300

    Update sale_order.py

--- html_description_field/models/sale_order.py
+++ html_description_field/models/sale_order.py
@@ -1,6 +1,6 @@
 
 # -*- coding: utf-8 -*-
-#© 2018 Trustcode / Adaptado pro Augusto D. Lisboa <augusto@bonamaison.com>
+#© 2018 Trustcode / Adaptado por Augusto D. Lisboa <augusto@bonamaison.com>
 #License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api
