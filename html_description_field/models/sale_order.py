96708a289bae13bc5ceb39c4bd80b18919e470b1
Augusto D. Lisbôa <augusto.diniz.l@gmail.com>
2018-10-03 17:16:43 -0300

    Update sale_order.py

--- html_description_field/models/sale_order.py
+++ html_description_field/models/sale_order.py
@@ -1,6 +1,6 @@
 
 # -*- coding: utf-8 -*-
-#© 2018 Trustcode / Adaptado por Augusto D. Lisboa <augusto@bonamaison.com>
+#© 2018 Trustcode / Adaptado por Augusto D. Lisbôa
 #License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api
@@ -23,5 +23,5 @@ class SaleOrderLine(models.Model):
     @api.onchange('product_id')
     def onchange_product_id(self):
         self.quotation_description = self.product_id.display_name
-        if self.product_id.variant_description_sale:
+		if self.product_id.variant_description_sale:
             self.quotation_description = self.product_id.variant_description_sale
