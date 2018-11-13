e18df26ef67de16e56b3aa8efc0ca846175907d7
marinaGD <mgd.marinadomingues@gmail.com>
2018-11-13 15:54:46 -0200

    add fields to invoice, fix observation not updating

--- commitment_requisition_date/models/__init__.py
+++ commitment_requisition_date/models/__init__.py
@@ -3,3 +3,4 @@ from . import stock_picking
 from . import procurement_rule
 from . import stock_move
 from . import sale_order
+from . import account_invoice
--- commitment_requisition_date/models/sale_order.py
+++ commitment_requisition_date/models/sale_order.py
@@ -13,42 +13,56 @@ class SaleOrder(models.Model):
     observation_sale_order = fields.Text(
         'Observation', track_visibility='onchange')
 
-    @api.onchange('observation_sale_order')
-    def _update_observation(self):
-        for pcking in self.picking_ids.filtered(lambda p: p.state != 'cancel'):
-            pcking.write(
-                {'observation_sale_order': self.observation_sale_order})
+    def _sale_order_dates_update(self):
+        self.update_account_invoice()
+        self.update_mrp_production()
+        self.update_stock_picking()
 
-        mrp_production = self.env['mrp.production'].search([
-                ('procurement_group_id', '=', self.procurement_group_id.id)])
-        for production in mrp_production:
-            production.write(
-                {'observation_sale_order': self.observation_sale_order})
-
-    def _sale_order_dates_update(self, vals):
+    def update_mrp_production(self):
         if self.procurement_group_id:
             mrp_production = self.env['mrp.production'].search([
                 ('procurement_group_id', '=', self.procurement_group_id.id)])
             for production in mrp_production:
-                production.update({
-                    'commitment_date': vals.get('commitment_date'),
-                    'requisition_date': vals.get('requested_date')
+                production.write({
+                    'commitment_date': self.commitment_date,
+                    'requisition_date': self.requested_date,
+                    'observation_sale_order': self.observation_sale_order
                 })
 
+    def update_account_invoice(self):
+        for inv in self.invoice_ids.filtered(lambda p: p.state != 'cancel'):
+            inv.write({
+                'commitment_date': self.commitment_date,
+                'requisition_date': self.requested_date,
+                'observation_sale_order': self.observation_sale_order
+            })
+
+    def update_stock_picking(self):
         for pcking in self.picking_ids.filtered(lambda p: p.state != 'cancel'):
             pcking.write({
-                'commitment_date': vals.get('commitment_date'),
-                'requisition_date': vals.get('requested_date')
+                'commitment_date': self.commitment_date,
+                'requisition_date': self.requested_date,
+                'observation_sale_order': self.observation_sale_order
             })
 
     @api.multi
     def write(self, vals):
         res = super(SaleOrder, self).write(vals)
-        if any(item in vals for item in ['commitment_date', 'requested_date']):
-            self._sale_order_dates_update({
-                'commitment_date': vals.get('commitment_date'),
-                'requested_date': vals.get('requested_date')
-            })
+        if any(item in vals for item in
+                ['commitment_date',
+                 'requested_date',
+                 'observation_sale_order']):
+            self._sale_order_dates_update()
+        return res
+
+    @api.multi
+    def _prepare_invoice(self):
+        res = super(SaleOrder, self)._prepare_invoice()
+        res.update({
+            'commitment_date': self.commitment_date,
+            'requested_date': self.requested_date,
+            'observation_sale_order': self.observation_sale_order,
+        })
         return res
 
     @api.multi
