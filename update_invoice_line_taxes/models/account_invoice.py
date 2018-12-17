83688a7dcf105d61992c6c016ea4a1780920bd5f
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-12-17 21:54:12 -0200

    [FIX] Preço unitário estava sendo perdido, pois o onchange do prduto sobrescreve o valor
    	modified:   update_invoice_line_taxes/models/account_invoice.py

--- update_invoice_line_taxes/models/account_invoice.py
+++ update_invoice_line_taxes/models/account_invoice.py
@@ -8,12 +8,20 @@ class AccountInvoice(models.Model):
         fpos = self.fiscal_position_id
         if fpos:
             for line in self.invoice_line_ids:
+                price_unit = line.price_unit
+                if price_unit == 0.0:
+                    continue
+                account_analytic_id = line.account_analytic_id.id
                 self.clear_line_tax_ids(line)
                 line._set_taxes_from_fiscal_pos()
                 line._set_taxes()
                 line._onchange_product_id()
                 line._br_account_onchange_product_id()
                 line._set_extimated_taxes(line.price_unit)
+                line.write({
+                    'price_unit': price_unit,
+                    'account_analytic_id': account_analytic_id
+                })
 
     def clear_line_tax_ids(self, line):
         line.tem_difal = False
