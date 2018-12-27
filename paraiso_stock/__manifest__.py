1a2acebf92688d5408bbf3826f7f1c0dc66f0d24
marinaGD <mgd.marinadomingues@gmail.com>
2018-12-27 14:30:15 -0200

    add responsible for shipping and colors in stock.move lives

---
+++ paraiso_stock/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# © 2017 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{  # pylint: disable=C8101,C8103
+    'name': "Paraíso Stock",
+
+    'summary': """
+        Altera visualização do processo de entrega""",
+
+    'description': """
+        Bloqueia a impressão da ordem de separação;
+        Atribui cores às linhas dos pedidos relacionando ao estoque;
+        Atribui responsável pelas entregas.
+    """,
+    'author': "Trustcode",
+    'website': "http://www.trustcode.com.br",
+    'category': 'Uncategorized',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'contributors': [
+        'Marina Domingues <mgd.marinadomingues@gmail.com>',
+    ],
+
+    'depends': ['sale', 'stock', 'br_delivery'],
+    'data': [
+        "views/stock_move_view.xml",
+        "views/stock_picking_view.xml",
+        "views/res_partner_view.xml",
+    ],
+
+}
