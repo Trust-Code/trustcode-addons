2da230db4a5a82449208111d375be90470f831f4
Danimar Ribeiro <danimaribeiro@gmail.com>
2019-07-19 21:08:23 -0300

    [ADD] Migrate existing modules do version 12

---
+++ import_serial_from_xlsx/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2019 Danimar Ribeiro <danimaribeiro@gmail.com>, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{   # pylint: disable=C8101,C8103
+    'name': 'Importação de Números seriais via XLSX',
+    'summary': """Importação de Números seriais via XLSX
+    Mantido por Trustcode""",
+    'version': '12.0.1.0.0',
+    'category': 'Stock',
+    'author': 'Danimar Ribeiro',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_pack_operation.xml',
+    ],
+}
---
+++ import_serial_from_xlsx/models/stock_pack_operation.py
@@ -0,0 +1,64 @@
+# © 2019 Danimar Ribeiro <danimaribeiro@gmail.com>, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import base64
+from io import BytesIO
+from openpyxl import load_workbook
+
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+
+
+# class StockPackOperationLot(models.Model):
+#     _inherit = 'stock.pack.operation.lot'
+#
+#     @api.model
+#     def create(self, vals):
+#         if "lot_name" in vals and "operation_id" in vals and vals['lot_name']:
+#             op = self.env['stock.pack.operation'].browse(vals['operation_id'])
+#             total = self.search_count(
+#                 [('lot_name', '=', vals['lot_name']),
+#                  ('operation_id', '=', op.id)])
+#             if total > 0:
+#                 raise UserError(
+#                     u'Você já mencionou este nome de lote em outra linha: %s' %
+#                     vals['lot_name'])
+#         return super(StockPackOperationLot, self).create(vals)
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    serials_xlsx = fields.Binary(string="XLS de Lotes")
+
+    @api.onchange('serials_xlsx')
+    def _onchange_serials_xlsx(self):
+        if not self.serials_xlsx:
+            return
+        xlsx = base64.b64decode(self.serials_xlsx.encode('utf-8'))
+        xlsx = BytesIO(xlsx)
+        wb = load_workbook(filename=xlsx)
+        lot_names = []
+        for sheet in wb.worksheets:
+            for row in sheet.rows:
+                if len(row) < 2:
+                    continue
+                for x in range(1, len(row), 3):
+                    if row[x].value:
+                        lot_names.append(row[x].value)
+
+        self.serials_xlsx = None
+        for lote in lot_names:
+            line_id = self.move_line_ids.filtered(
+                lambda x: x.lot_id.name == lote or x.lot_name == lote)
+            if line_id:
+                line_id.qty_done += 1
+            else:
+                line_id = self.move_line_ids.filtered(
+                    lambda x: not x.lot_name and not x.lot_id)
+                if line_id:
+                    lot_id = self.env['stock.production.lot'].search(
+                        [('name', '=', lote),
+                         ('product_id', '=', self.product_id.id)])
+                    line_id[0].update({'lot_name': lote, 'qty_done': 1,
+                                       'lot_id': lot_id.id})
