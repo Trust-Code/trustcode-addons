40be91bf1d0c36e0456ebc185796a3f8b5bedc0c
Danimar Ribeiro <danimaribeiro@gmail.com>
2019-08-16 10:54:41 -0300

    [ADD] Módulo que calcula a última data de pagamento

---
+++ invoice_payment_date/__manifest__.py
@@ -0,0 +1,21 @@
+# © 2019 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{  # pylint: disable=C8101,C8103
+    'name': "Last payment date in Invoice",
+    'author': "Trustcode",
+    'website': "http://www.trustcode.com.br",
+    'category': 'Account',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'views/account_invoice.xml',
+    ],
+}
