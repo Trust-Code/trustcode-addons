ef492bac56baba17b846ed70e1bab4906176cb59
Danimar Ribeiro <danimaribeiro@gmail.com>
2019-08-16 16:12:57 -0300

    [ADD] Módulo que adiciona campos extras nas despesas

---
+++ hr_expense_approved_by/models/hr_expense.py
@@ -0,0 +1,9 @@
+
+from odoo import api, fields, models
+
+
+class HrExpense(models.Model):
+    _inherit = 'hr.expense'
+
+    responsible_id = fields.Many2one(
+        'res.users', related='sheet_id.responsible_id', readonly=True)
