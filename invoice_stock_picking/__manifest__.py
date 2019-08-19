9f6825a7b1e2107a23c9610fa87726d1fedca6ba
Mackilem <mack.vdl@gmail.com>
2019-08-19 03:15:55 -0300

    correções pylint

--- invoice_stock_picking/__manifest__.py
+++ invoice_stock_picking/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # © 2017 Fábio Luna <fabiocluna@hotmail.com>, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {   # pylint: disable=C8101,C8103
     'name': 'Fatura Gera Picking',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Account addons',
-    'license': 'OPL-1',
+    'license': 'AGPL-3.0',
     'author': 'Trustcode',
     'website': 'http://www.trustcode.com.br',
     'description': """
