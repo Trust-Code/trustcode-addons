c419ac6e59a3939c6c3bf14628edde45716f44cc
Danimar Ribeiro <danimaribeiro@gmail.com>
2020-06-09 23:13:01 -0300

    Call the onchange of all the taxes

--- update_invoice_line_taxes/models/account_invoice.py
+++ update_invoice_line_taxes/models/account_invoice.py
@@ -1,5 +1,9 @@
 from odoo import models
 
+TAXES = ['icms', 'icms_st', 'icms_fcp', 'icms_inter',
+         'icms_intra', 'ipi', 'pis', 'cofins',
+         'issqn', 'ii', 'irrf', 'csll', 'inss']
+
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
@@ -18,18 +22,27 @@ class AccountInvoice(models.Model):
                 line._onchange_product_id()
                 line._br_account_onchange_product_id()
                 line._set_extimated_taxes(line.price_unit)
+                self._set_all_taxes_amount(line)
                 line.write({
                     'price_unit': price_unit,
                     'account_analytic_id': account_analytic_id
                 })
             self._onchange_invoice_line_ids()
 
+    def _set_all_taxes_amount(self, line):
+        for tax in TAXES:
+            str_onchange = '_onchange_tax_%s_id' % tax
+            onchange = getattr(line, str_onchange)
+            if onchange:
+                onchange()
+
     def clear_line_tax_ids(self, line):
         line.tem_difal = False
-        taxes = ['icms', 'ipi', 'pis', 'cofins',
-                 'issqn', 'ii', 'irrf', 'csll', 'inss']
-        for tax in taxes:
-            line.update({
-                ('tax_%s_id' % tax): False,
-                ('%s_rule_id' % tax): False})
-        line.update({'tax_icms_st_id': False})
+
+        vals = {}
+        for tax in TAXES:
+            vals['tax_%s_id' % tax] = False
+            if tax not in ('icms_st', 'icms_fcp', 'icms_inter', 'icms_intra'):
+                vals['%s_rule_id' % tax] = False
+
+        line.update(vals)
