5e146b6065fab5b51cb9c2060f5358b2501186c8
Danimar Ribeiro <danimaribeiro@gmail.com>
2020-11-18 13:14:50 -0300

    Improve performance disabling recompute when updating taxes

--- update_invoice_line_taxes/models/account_invoice.py
+++ update_invoice_line_taxes/models/account_invoice.py
@@ -11,23 +11,25 @@ class AccountInvoice(models.Model):
     def update_taxes(self):
         fpos = self.fiscal_position_id
         if fpos and self.state == 'draft':
-            for line in self.invoice_line_ids:
-                price_unit = line.price_unit
-                if price_unit == 0.0:
-                    continue
-                account_analytic_id = line.account_analytic_id.id
-                self.clear_line_tax_ids(line)
-                line._set_taxes_from_fiscal_pos()
-                line._set_taxes()
-                line._onchange_product_id()
-                line._br_account_onchange_product_id()
-                line._set_extimated_taxes(line.price_unit)
-                self._set_all_taxes_amount(line)
-                line.write({
-                    'price_unit': price_unit,
-                    'account_analytic_id': account_analytic_id
-                })
-            self._onchange_invoice_line_ids()
+            with self.env.norecompute():
+                for line in self.invoice_line_ids:
+                    price_unit = line.price_unit
+                    if price_unit == 0.0:
+                        continue
+                    account_analytic_id = line.account_analytic_id.id
+                    self.clear_line_tax_ids(line)
+                    line._set_taxes_from_fiscal_pos()
+                    line._set_taxes()
+                    line._onchange_product_id()
+                    line._br_account_onchange_product_id()
+                    line._set_extimated_taxes(line.price_unit)
+                    self._set_all_taxes_amount(line)
+                    line.write({
+                        'price_unit': price_unit,
+                        'account_analytic_id': account_analytic_id
+                    })
+                self._onchange_invoice_line_ids()
+            self.recompute()
 
     def _set_all_taxes_amount(self, line):
         for tax in TAXES:
