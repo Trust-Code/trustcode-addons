948efaee783f50bc53b928206bbf3c1a6294e171
Danimar Ribeiro <danimaribeiro@gmail.com>
2021-08-05 17:58:23 -0300

    Fixes for the currency value

--- website_sale_currency_convert/models/product.py
+++ website_sale_currency_convert/models/product.py
@@ -6,10 +6,15 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     def _compute_brl_price(self):
-        brl_id = self.env['res.currency'].search([('name', '=', 'USD')])
+        brl_id = self.env['res.currency'].search([('name', '=', 'BRL')])
+        usd_id = self.env['res.currency'].search([('name', '=', 'USD')])
         for item in self:
-            item.brl_currency_id = brl_id.id
-            item.brl_list_price = brl_id.compute(item.list_price, brl_id)
+            dolar_price = item.item_ids.filtered(lambda x: x.currency_id == usd_id)
+            item.brl_currency_id = usd_id.id
+            if dolar_price:
+                item.brl_list_price = dolar_price.fixed_price
+            else:
+                item.brl_list_price = 0.0
 
     brl_currency_id = fields.Many2one('res.currency', compute="_compute_brl_price")
     brl_list_price = fields.Monetary(currency="brl_currency_id", compute="_compute_brl_price")
--- website_sale_currency_convert/models/sale_order.py
+++ website_sale_currency_convert/models/sale_order.py
@@ -5,11 +5,12 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    def _compute_brl_price(self):
-        brl_id = self.env['res.currency'].search([('name', '=', 'USD')])
+    def _compute_usd_price(self):
+        brl_id = self.env['res.currency'].search([('name', '=', 'BRL')])
+        usd_id = self.env['res.currency'].search([('name', '=', 'USD')])
         for item in self:
-            item.brl_currency_id = brl_id.id
-            item.brl_amount_total = brl_id.compute(item.amount_total, brl_id)
+            item.brl_currency_id = usd_id.id
+            item.brl_amount_total = brl_id.compute(item.amount_total, usd_id)
 
-    brl_currency_id = fields.Many2one('res.currency', compute="_compute_brl_price")
-    brl_amount_total = fields.Monetary(currency="brl_currency_id", compute="_compute_brl_price")
+    brl_currency_id = fields.Many2one('res.currency', compute="_compute_usd_price")
+    brl_amount_total = fields.Monetary(currency="brl_currency_id", compute="_compute_usd_price")
