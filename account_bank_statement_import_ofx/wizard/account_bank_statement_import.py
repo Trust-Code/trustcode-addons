d4e29ebfaa07ee29f47d60f34e67ca0342d7ef4e
Danimar Ribeiro <danimaribeiro@gmail.com>
2021-09-08 14:33:39 -0300

    Create a unique identifier for the ofx line combining id, value and date

--- account_bank_statement_import_ofx/wizard/account_bank_statement_import.py
+++ account_bank_statement_import_ofx/wizard/account_bank_statement_import.py
@@ -33,7 +33,7 @@ class AccountBankStatementImport(models.TransientModel):
         return ofx
 
     @api.model
-    def _prepare_ofx_transaction_line(self, transaction):
+    def _prepare_ofx_transaction_line(self, transaction, use_amount_as_identifier=False):
         # Since ofxparse doesn't provide account numbers,
         # we cannot provide the key 'bank_account_id',
         # nor the key 'account_number'
@@ -45,12 +45,18 @@ class AccountBankStatementImport(models.TransientModel):
             name += " " + transaction.checknum
         if transaction.memo:
             name += " : " + transaction.memo
+        unique_id = transaction.id
+        if use_amount_as_identifier:
+            unique_id = "%s-%.0d-%s" % (
+                unique_id, abs(float(transaction.amount) * 100),
+                transaction.date.strftime('%Y%m%d'))
+
         vals = {
             "date": transaction.date,
             "name": name,
             "ref": transaction.id,
             "amount": float(transaction.amount),
-            "unique_import_id": transaction.id,
+            "unique_import_id": unique_id,
         }
         return vals
 
@@ -62,8 +68,10 @@ class AccountBankStatementImport(models.TransientModel):
         transactions = []
         total_amt = 0.00
         try:
+            ids = [x.id for x in ofx.account.statement.transactions]
+            use_amount_as_identifier = len(ids) != len(set(ids))
             for transaction in ofx.account.statement.transactions:
-                vals = self._prepare_ofx_transaction_line(transaction)
+                vals = self._prepare_ofx_transaction_line(transaction, use_amount_as_identifier)
                 if vals:
                     transactions.append(vals)
                     total_amt += vals["amount"]
