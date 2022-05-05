36dba67f9d782b5839961aa324425a5f2059b316
Danimar Ribeiro <danimaribeiro@gmail.com>
2022-05-05 13:44:12 -0300

    Remove campo inexistente

--- boleto_cloud/models/account_move.py
+++ boleto_cloud/models/account_move.py
@@ -54,7 +54,6 @@ class AccountMove(models.Model):
                 'amount': moveline.amount_residual,
                 'currency_id': moveline.move_id.currency_id.id,
                 'partner_id': moveline.partner_id.id,
-                'type': 'server2server',
                 'date_maturity': moveline.date_maturity,
                 'invoice_ids': [(6, 0, self.ids)],
             })
