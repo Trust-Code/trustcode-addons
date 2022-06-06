8b1a97fd6322909483afc137357febb5a4e92ed6
Danimar Ribeiro <danimaribeiro@gmail.com>
2022-06-06 16:56:42 -0300

    Adicionad tipos de preco a tabela de atributos

--- pricelist_per_attribute/models/product_template.py
+++ pricelist_per_attribute/models/product_template.py
@@ -10,6 +10,8 @@ class ProductAttributeValuePrice(models.Model):
 
     min_quantity = fields.Float(string="Quantidade Mínima")
     fixed_price = fields.Float(string="Preço fixo")
+    price_type = fields.Selection([
+        ("unit", "Unitário"), ("thousand", "Milheiro"), ("fixed", "Fixo")], default="unit")
 
 
 class ProductTemplateAttributeValue(models.Model):
@@ -30,6 +32,21 @@ class ProductProduct(models.Model):
             total_price = 0
             for value in product.product_template_attribute_value_ids:
                 prices = value.value_price_ids.filtered(lambda x: x.min_quantity < quantity).sorted("min_quantity")
-                total_price += (prices and prices[0].fixed_price or 0) / quantity
+                if prices:
+
+                    price_type = prices[0].price_type
+
+                    if price_type == "unit":
+                        total_price += prices[0].fixed_price
+                    elif price_type == "thousand":
+                        multiplier = quantity // 1000
+                        remainder = quantity % 1000
+
+                        total_price += (prices[0].fixed_price / quantity) * multiplier
+                        if remainder:
+                            total_price += prices[0].fixed_price / remainder
+
+                    elif price_type == "fixed":
+                        total_price += prices[0].fixed_price / quantity
 
             product.price_extra = product.price_extra + total_price
