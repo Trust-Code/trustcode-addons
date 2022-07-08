823251809f532102cd6091993d605394615c191c
Danimar Ribeiro <danimaribeiro@gmail.com>
2022-07-08 12:21:06 -0300

    Adiciona o preco extra tambem ao standard price (custo)

--- pricelist_per_attribute/models/product_template.py
+++ pricelist_per_attribute/models/product_template.py
@@ -28,6 +28,44 @@ class ProductAttributeValue(models.Model):
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
+    def price_compute(self, price_type, uom=False, currency=False, company=None):
+        # TDE FIXME: delegate to template or not ? fields are reencoded here ...
+        # compatibility about context keys used a bit everywhere in the code
+        if not uom and self._context.get('uom'):
+            uom = self.env['uom.uom'].browse(self._context['uom'])
+        if not currency and self._context.get('currency'):
+            currency = self.env['res.currency'].browse(self._context['currency'])
+
+        products = self
+        if price_type == 'standard_price':
+            # standard_price field can only be seen by users in base.group_user
+            # Thus, in order to compute the sale price from the cost for users not in this group
+            # We fetch the standard price as the superuser
+            products = self.with_company(company or self.env.company).sudo()
+
+        prices = dict.fromkeys(self.ids, 0.0)
+        for product in products:
+            prices[product.id] = product[price_type] or 0.0
+            if price_type in ('list_price', 'standard_price'):
+                print(product.price_extra)
+                prices[product.id] += product.price_extra
+                # we need to add the price from the attributes that do not generate variants
+                # (see field product.attribute create_variant)
+                if self._context.get('no_variant_attributes_price_extra'):
+                    # we have a list of price_extra that comes from the attribute values, we need to sum all that
+                    prices[product.id] += sum(self._context.get('no_variant_attributes_price_extra'))
+
+            if uom:
+                prices[product.id] = product.uom_id._compute_price(prices[product.id], uom)
+
+            # Convert from current user company currency to asked one
+            # This is right cause a field cannot be in more than one currency
+            if currency:
+                prices[product.id] = product.currency_id._convert(
+                    prices[product.id], currency, product.company_id, fields.Date.today())
+
+        return prices
+
     def _compute_product_price_extra(self):
         super(ProductProduct, self)._compute_product_price_extra()
         for product in self:
@@ -38,7 +76,7 @@ class ProductProduct(models.Model):
             for prd_value in product.product_template_attribute_value_ids:
 
                 attr_value = prd_value.product_attribute_value_id
-                prices = attr_value.value_price_ids.filtered(lambda x: x.min_quantity < quantity).sorted("min_quantity", reverse=True)
+                prices = attr_value.value_price_ids.filtered(lambda x: x.min_quantity <= quantity).sorted("min_quantity", reverse=True)
                 if prices:
                     price_type = prices[0].price_type
 
